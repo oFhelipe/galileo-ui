39775d1515cfff6680c4be4989b9f35f6a3af051
Fhelipe Rodrigues <contatofhelipe@gmail.com>
2025-01-11 11:16:06 -0300

    feat: init design system with configurations and tokens

---
+++ packages/eslint-config/react.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: "@rocketseat/eslint-config/react",
+  rules: {
+    "react/no-unescaped-entities": "off",
+  },
+};
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <span>ola mundo</span>;
+};
+
+export default App;
