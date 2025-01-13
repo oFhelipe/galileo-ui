4457df9e7e2927317619e5897b4ee0a2ab1f9a7e
Fhelipe Rodrigues <contatofhelipe@gmail.com>
2025-01-13 00:05:20 -0300

    fix: fix storybook init in all systems

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,13 +1,14 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
-import { join, dirname } from "path";
-
+import { join, resolve, dirname } from "path";
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
+function getAbsolutePath(value: string): string {
+  return dirname(
+    resolve(join("..", "..", "node_modules", value, "package.json"))
+  );
 }
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
