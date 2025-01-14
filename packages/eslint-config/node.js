19157a568dda2cb9407650723f10c134c86ace15
Fhelipe Rodrigues <contatofhelipe@gmail.com>
2025-01-13 21:05:42 -0300

    style: fix eslint prettier conflict errors

--- packages/eslint-config/node.js
+++ packages/eslint-config/node.js
@@ -1,3 +1,22 @@
 module.exports = {
   extends: "@rocketseat/eslint-config/node",
+  plugins: ["prettier"],
+  rules: {
+    "react/no-unescaped-entities": "off",
+    rules: {
+      "react/self-closing-comp": "error",
+      "prettier/prettier": [
+        "error",
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          singleQuote: false,
+          trailingComma: "all",
+          arrowParens: "always",
+          semi: false,
+          endOfLine: "auto",
+        },
+      ],
+    },
+  },
 };
--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -1,6 +1,21 @@
 module.exports = {
   extends: "@rocketseat/eslint-config/react",
+  plugins: ["prettier"],
   rules: {
     "react/no-unescaped-entities": "off",
+    rules: {
+      "react/self-closing-comp": "error",
+      "prettier/prettier": [
+        "error",
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          trailingComma: "all",
+          arrowParens: "always",
+          semi: false,
+          endOfLine: "auto",
+        },
+      ],
+    },
   },
 };
